3ab212ef6afa78b8c916a3eb8f6eaa1465ab05b0
KostasTritsonis <ktritsonis@hotmail.com>
2025-01-03 16:37:21 +0200

    03012025

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import './Navbar.css'
 import logo from '../../assets/logo.png'
 import { Link } from 'react-scroll'
 import menu_icon from '../../assets/menu-icon.png'
+import greek from '../../assets/greek.png'
+import english from '../../assets/english.png'
 
 const Navbar = () => {
   
@@ -20,15 +22,19 @@ const Navbar = () => {
     mobileMenu ? setMobleMenu(false) : setMobleMenu(true);
   } 
 
+  const [flag,setflag] = useState(true);
+
   return (
     <nav className={`container ${sticky ? 'dark-nav':''}`} >
         <img src={logo} alt="" className='logo' />
         <p>MATINA MAKRYGIANNI</p>
         <ul className={mobileMenu ?'':'hide-mobile-menu'}>
-            <li><Link href='#hero' to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+            <li><button onClick={() => setflag(!flag)}><img src={flag ? english : greek} alt="" /></button></li>
+            <li><Link href='#hero' to='hero' smooth={true} offset={0} duration={500}></Link></li>
             <li><Link href='#programs' to='programs' smooth={true} offset={-260} duration={500}>Program</Link></li>
             <li><Link href='#about' to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
             <li><Link href='#contact' to='contact' smooth={true} offset={-260} duration={500}><button className='btn'>Contact us</button></Link></li>
+            
         </ul>
         <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
