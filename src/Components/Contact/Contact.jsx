bd80462e8026594fe9f3f5e675e1832075ba5e99
KostasTritsonis <ktritsonis@hotmail.com>
2025-01-03 13:56:08 +0200

    03012025

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Contact.css'
-import msg_icon from '../../assets/msg-icon.png'
 import mail_icon from '../../assets/mail-icon.png'
 import phone_icon from '../../assets/phone-icon.png'
 import location_icon from '../../assets/location-icon.png'
@@ -10,7 +9,7 @@ const Contact = () => {
   return (
     <div className='contact'>
         <div className="contact-col">
-            <h3>Send us a message <img src={msg_icon} alt="" /></h3>
+            <h3>Send us a message </h3>
             <p>Feel free to reach out through contact form or find our contact
             information below. Your feedback, questions, and suggestions are important
             to us as we strive to provide exceptional service to our community.</p>
--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -3,38 +3,66 @@ import './Programs.css'
 import program_1 from '../../assets/cambridge.png'
 import program_2 from '../../assets/michigan.svg'
 import program_3 from '../../assets/ielts.png'
-import program_icon_1 from '../../assets/program-icon-1.png'
-import program_icon_2 from '../../assets/program-icon-2.png'
-import program_icon_3 from '../../assets/program-icon-3.png'
+import program_icon_1 from '../../assets/excellence.png'
+import program_icon_2 from '../../assets/advancement.png'
+import program_icon_3 from '../../assets/global.png'
+import level from '../../assets/englishlvl.avif'
 
 
 
 const Programs = () => {
   return (
+    <>
     <div className='programs' >
         <div className="program">
             <img src={program_1} alt="" />
             <div className="caption">
                 <img src={program_icon_1} alt="" />
-                <p>Graduation Degree</p>
+                <p>Excellence</p>
             </div>
         </div>
         <div className="program">
             <img src={program_2} alt="" />
             <div className="caption">
                 <img src={program_icon_2} alt="" />
-                <p>Master Degree</p>
+                <p>Advancement</p>
             </div>
         </div>
         <div className="program">
-            <img className="ielts" src={program_3} alt="" />
+            <img src={program_3} alt="" />
             <div className="caption">
                 <img src={program_icon_3} alt="" />
-                <p>Post Graduation</p>
+                <p>Global Opportunity</p>
             </div>
         </div>
-        
     </div>
+    <p className='program-title'>English Certification: Your Roadmap to Success!</p>
+    <div className="levels">
+        <div className="program-left">
+            <img src={level} alt="" />
+        </div>
+        <div className="program-right">
+            <p><b>A1 English (Beginner/Elementary)</b>
+            Level A1 corresponds to basic users of English who can understand and
+            use familiar everyday expressions and very basic phrases.</p>
+            <p><b>A2 English (Pre Intermediate)</b>
+            Level A2 corresponds to those users who can understand basic expressions
+            and communicate in a simple manner.</p>
+            <p><b>B1 English (Intermediate)</b>
+            Level B1 corresponds to users who can understand and produce text on familiar
+            topics and give opinions and descriptions.</p>
+            <p><b>B2 English (Upper Intermediate)</b>
+            Level B2 corresponds to users who can produce clear, detailed text and
+            interact with a degree of fluency and spontaneity.</p>
+            <p><b>C1 English (Advanced)</b>
+            Level C1 corresponds to users who can express themselves fluently and spontaneously.
+            They can use language flexibly and effectively for all purposes.</p>
+            <p><b>C2 English (Proficient)</b>
+            Level C2 corresponds to proficient users of English, who can understand and express
+            virtually everything with ease and differentiate finer shades of meaning.</p>
+        </div>
+    </div>
+    </>
   )
 }
 
