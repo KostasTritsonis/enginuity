9025a088e80a8179c548a7978f507a596be1f92f
KostasTritsonis <ktritsonis@hotmail.com>
2025-01-03 14:26:14 +0200

    03012025

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -25,10 +25,10 @@ const Navbar = () => {
         <img src={logo} alt="" className='logo' />
         <p>ENGINUITY</p>
         <ul className={mobileMenu ?'':'hide-mobile-menu'}>
-            <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
-            <li><Link to='programs' smooth={true} offset={-260} duration={500}>Program</Link></li>
-            <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
-            <li><Link to='contact' smooth={true} offset={-260} duration={500}><button className='btn'>Contact us</button></Link></li>
+            <li><Link href='#hero' to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+            <li><Link href='#programs' to='programs' smooth={true} offset={-260} duration={500}>Program</Link></li>
+            <li><Link href='#about' to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
+            <li><Link href='#contact' to='contact' smooth={true} offset={-260} duration={500}><button className='btn'>Contact us</button></Link></li>
         </ul>
         <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
